be7751c79a50a25eaba70e208e6c712804c69583
Toptachamann <timofey.chudakov@gmail.com>
2018-03-30 10:05:03 +0300

    Hibernate ApplicationDaoImpl

--- src/main/java/app/dao/hibernate/ApplicationDaoImpl.java
+++ src/main/java/app/dao/hibernate/ApplicationDaoImpl.java
@@ -5,21 +5,65 @@ import app.model.Algorithm;
 import app.model.Application;
 import app.model.AreaOfUse;
 
-import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
-public class ApplicationDaoImpl  extends AbstractDao implements ApplicationDao{
+public class ApplicationDaoImpl extends AbstractDao implements ApplicationDao {
   @Override
-  public Application createApplication(Algorithm algorithm, AreaOfUse areaOfUse) throws SQLException {
-    return null;
+  public Application createApplication(Algorithm algorithm, AreaOfUse areaOfUse) {
+    Application application = new Application(algorithm, areaOfUse);
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    entityManager.persist(application);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return application;
   }
 
   @Override
-  public boolean containsApplication(Algorithm algorithm, AreaOfUse areaOfUse) throws SQLException {
-    return false;
+  public boolean containsApplication(Algorithm algorithm, AreaOfUse areaOfUse) {
+    EntityManager entityManager = getEntityManager();
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Application> query = builder.createQuery(Application.class);
+    Root<Application> root = query.from(Application.class);
+    query.where(
+        builder.and(
+            builder.equal(root.get(Application_.algorithm), algorithm),
+            builder.equal(root.get(Application_.areaOfUse), areaOfUse)));
+    List<Application> result = entityManager.createQuery(query).getResultList();
+    entityManager.close();
+    return !result.isEmpty();
   }
 
   @Override
-  public void deleteApplication(Algorithm algorithm, AreaOfUse areaOfUse) throws SQLException {
+  public void deleteApplication(Algorithm algorithm, AreaOfUse areaOfUse) {
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaDelete<Application> delete = builder.createCriteriaDelete(Application.class);
+    Root<Application> root = delete.from(Application.class);
+    delete.where(
+        builder.and(
+            builder.equal(root.get(Application_.algorithm), algorithm),
+            builder.equal(root.get(Application_.areaOfUse), areaOfUse)));
+    entityManager.createQuery(delete).executeUpdate();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+  }
 
+  @Override
+  public List<Application> getApplicationsByArea(AreaOfUse area) {
+    EntityManager entityManager = getEntityManager();
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Application> query = builder.createQuery(Application.class);
+    Root<Application> root = query.from(Application.class);
+    query.where(builder.equal(root.get(Application_.areaOfUse), area));
+    List<Application> result = entityManager.createQuery(query).getResultList();
+    entityManager.close();
+    return result;
   }
 }
